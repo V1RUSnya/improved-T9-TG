9c7e712e669e6550d02273b9359bf4d817b90c76
V1RUSnya <dima.nesed@gmail.com>
2024-06-26 00:17:17 +0300

    fix checker.can_correct()

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ def typing(client_object, message: pyrogram.types.Message):
     if message.text == "/start":
         user_id = message.from_user.id
         message.edit("Ready ☑")
-    elif user_id is not None and message.from_user.id == user_id and checker.can_correct():
-        words = message.text.split()
-        corrected_words = []
-        for word in words:
-            result = checker.check_word(word)
-            if result:
-                corrected_words.append(result)
-            else:
-                corrected_words.append(word)
-        corrected_text = ' '.join(corrected_words)
-        print(f"{message.text} -->> {corrected_text}")
-        if message.text != corrected_text:
-            message.edit(corrected_text)
+    elif user_id is not None:
+        if message.from_user.id == user_id:
+            words = message.text.split()
+            corrected_words = []
+            for word in words:
+                result = checker.check_word(word)
+                if result:
+                    corrected_words.append(result)
+                else:
+                    corrected_words.append(word)
+            corrected_text = ' '.join(corrected_words)
+            if message.text != corrected_text and checker.can_correct():
+                print(f"{message.text} -->> {corrected_text}")
+                message.edit(corrected_text)
     checker.save_db()
 
 
