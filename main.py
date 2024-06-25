48a63ec0bbe97f412e87d33729d3cc5137007a05
V1RUSnya <dima.nesed@gmail.com>
2024-06-25 23:19:09 +0300

    Return to Python

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import correct
 
 checker = correct.WordChecker()
 app = pyrogram.Client("my_account")
-your_user_id = 774159671  # ID телеграмм аккаунта
+your_user_id = 774159671  # ID user
+
 
 @app.on_message()
-def type(client_object, message: pyrogram.types.Message):
+def typing(client_object, message: pyrogram.types.Message):
     if message.from_user is not None and message.from_user.id == your_user_id:
         words = message.text.split()
         corrected_words = []
         for word in words:
             result = checker.check_word(word)
-            if result != False:
+            if result:
                 corrected_words.append(result)
             else:
                 corrected_words.append(word)
@@ -20,5 +21,6 @@ def type(client_object, message: pyrogram.types.Message):
         print(f"{message.text} -->> {corrected_text}")
         if message.text != corrected_text: message.edit(corrected_text)
     checker.save_db()
+
 
 app.run()
